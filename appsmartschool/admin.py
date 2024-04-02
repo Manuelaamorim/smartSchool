9a75c68a21b44ee083b2f3afc9050f6f5af68726
luzianes <luzianes@gmail.com>
2024-04-02 20:04:04 -0300

    formulario_contato

--- appsmartschool/admin.py
+++ appsmartschool/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 
 from .models import UserAluno
 from .models import Dados_saude
+from .models import MensagemContato
 
 admin.site.register(UserAluno)
 admin.site.register(Dados_saude)
+admin.site.register(MensagemContato)
 
--- appsmartschool/urls.py
+++ appsmartschool/urls.py
@@ -2,10 +2,12 @@ from django import urls
 from django.urls import path
 from .views import login
 from .views import dados_saude_visualizar
+from .views import formulario_contato
 
 app_name= 'appsmartschool'
 urlpatterns = [
     path('auth/', login, name='login'),
     path('dadossaude/', dados_saude_visualizar, name='dados-saude'),
+    path('contato/', formulario_contato, name='formulario_contato'),
     # Certifique-se de adicionar outras URLs conforme necessário
 ]
