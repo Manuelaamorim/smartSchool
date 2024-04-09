98a55119275db8fa1838b26883a079dc679704a9
Manuela Cavalcanti <manucamorim@hotmail.com>
2024-04-08 22:19:12 -0300

    fixing width 3

--- dadosdesaude/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Dados_saude(models.Model):
-    peso = models.FloatField(max_length=4, null=False)
-    altura = models.FloatField(max_length = 4, null=False)
-    restricao_alimentar = models.TextField(max_length = 100)
-    tdah = models.CharField(max_length = 4)
-    pcd = models.CharField(max_length = 4)
-
