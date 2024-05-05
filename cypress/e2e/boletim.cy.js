e266dd92df721dad66eab8e088d08b70cb96d02d
Manuela Cavalcanti <manucamorim@hotmail.com>
2024-05-05 12:19:00 -0300

    finalizando testes

--- cypress/e2e/boletim.cy.js
+++ cypress/e2e/boletim.cy.js
@@ -1,5 +1,61 @@
 describe('test visualizar Boletim', () => {
     it('cenario1', () => { //Maria Ferreira está cadastrada no sistema da SmartSchool e está matriculada nas aulas do 3º ano C do Fundamental I, que inclui as matérias de História III, Geografia III, Matemática III, Português III, e Ciências III. Ela tentará consultar as suas notas e o banco de dados retornará as informações salvas referentes a todas as notas por disciplina da aluna;
+
+        cy.visit('/admin'); //pagina http://127.0.0.1:8000/
+        cy.get('#id_username').type('admin')
+        cy.get('#id_password').type('123456')
+        cy.get('.submit-row > input').click()
+        cy.get('.model-user > :nth-child(2) > .addlink').click()
+        cy.get('#id_username').type('12476892303')
+        cy.get('#id_password1').type('mar12345')
+        cy.get('#id_password2').type('mar12345')
+        cy.get('.default').click()
+
+        cy.get('.model-useraluno > td > .addlink')
+        cy.get('.model-useraluno > td > .addlink').click()
+        cy.get('#id_user').select('12476892303');
+        cy.get('#id_matricula').type('mar12345')
+        cy.get('#id_nome').type('Maria Ferreira')
+        cy.get('#id_cpf').type('12476892303')
+        cy.get('#id_data_de_nascimento').type('01/01/2016')
+        cy.get('#id_endereco').type('Rua da alegria, 23')
+        cy.get('#id_colegio').type('Santa Cruz')
+        cy.get('#id_serie').type('3')
+        cy.get('#id_turma').type('C')
+        cy.get('#id_email').type('antoniaferreira@gmail.com')
+        cy.get('#id_nome_responsavel').type('Antônia Maria Ferreira')
+        cy.get('#id_cpf_responsavel').type('98765434210')
+        cy.get('#id_telefone').type('81940028926')
+        cy.get('#id_email_responsavel').type('antoniaferreira@gmail.com')
+        cy.get('.default').click()
+
+        cy.get('.model-notas > th').click()
+        cy.get('li > .addlink').click()
+        cy.get('#id_aluno').select('Aluno Maria Ferreira')
+        cy.get('#id_materia_1').clear()
+        cy.get('#id_materia_1').type('História')
+        cy.get('#id_materia_2').type('Geografia')
+        cy.get('#id_materia_3').type('Matemática')
+        cy.get('#id_materia_4').type('Português')
+
+        cy.get('#id_nota1_materia1').type('8.5')
+        cy.get('#id_nota2_materia1').type('9.0')
+        cy.get('#id_nota3_materia1').type('10.0')
+
+        cy.get('#id_nota1_materia2').type('7.3')
+        cy.get('#id_nota2_materia2').type('8.4')
+        cy.get('#id_nota3_materia2').type('9.5')
+
+        cy.get('#id_nota1_materia3').type('10.0')
+        cy.get('#id_nota2_materia3').type('7.0')
+        cy.get('#id_nota3_materia3').type('8.0')
+
+        cy.get('#id_nota1_materia4').type('7.5')
+        cy.get('#id_nota2_materia4').type('9.2')
+        cy.get('#id_nota3_materia4').type('9.8')
+
+        cy.get('.default').click()
+
         cy.visit('/');
         cy.get('[href="/aluno/"] > .botao').click()
         cy.get('#username').type('12476892303')
@@ -12,6 +68,60 @@ describe('test visualizar Boletim', () => {
 
     it('cenario2', () => { //Luiz Rosa está cadastrado no sistema da SmartSchool com os seguintes dados: matrícula - 20090304; CPF - 59843216754; data de nascimento - 24/05/2018; endereço - Rua Padre Roma 345; e-mail - germanarosa@gmail.com; nome do responsável - Germana Rosa; CPF do responsável - 94325674387 e está matriculado nas aulas do 1º ano B do Fundamental I, que inclui as matérias de História I, Geografia I, Matemática I, Português I e Ciências I. Ele poderá consultar apenas as notas de cada matéria em que está matriculado, e não as de Maria Ferreira, ainda que ela esteja na mesma turma;
 
+        cy.visit('/admin'); //pagina http://127.0.0.1:8000/
+        cy.get('#id_username').type('admin')
+        cy.get('#id_password').type('123456')
+        cy.get('.submit-row > input').click()
+        cy.get('.model-user > :nth-child(2) > .addlink').click()
+        cy.get('#id_username').type('59843216754')
+        cy.get('#id_password1').type('lui20090304')
+        cy.get('#id_password2').type('lui20090304')
+        cy.get('.default').click()
+
+        cy.get('.model-useraluno > td > .addlink')
+        cy.get('.model-useraluno > td > .addlink').click()
+        cy.get('#id_user').select('59843216754');
+        cy.get('#id_matricula').type('lui20090304')
+        cy.get('#id_nome').type('Luiz Rosa')
+        cy.get('#id_cpf').type('59843216754')
+        cy.get('#id_data_de_nascimento').type('24/05/2016')
+        cy.get('#id_endereco').type(' Rua Padre Roma 345')
+        cy.get('#id_colegio').type('Novo Elemento')
+        cy.get('#id_serie').type('3')
+        cy.get('#id_turma').type('C')
+        cy.get('#id_email').type('germanarosa@gmail.com')
+        cy.get('#id_nome_responsavel').type('Germana Rosa')
+        cy.get('#id_cpf_responsavel').type('94325674387')
+        cy.get('#id_telefone').type('81985676822')
+        cy.get('#id_email_responsavel').type('germanarosa@gmail.com')
+        cy.get('.default').click()
+
+        cy.get('.model-notas > th').click()
+        cy.get('li > .addlink').click()
+        cy.get('#id_aluno').select('Aluno Luiz Rosa')
+        cy.get('#id_materia_1').type('História')
+        cy.get('#id_materia_2').type('Geografia')
+        cy.get('#id_materia_3').type('Matemática')
+        cy.get('#id_materia_4').type('Português')
+
+        cy.get('#id_nota1_materia1').type('6.5')
+        cy.get('#id_nota2_materia1').type('8.3')
+        cy.get('#id_nota3_materia1').type('9.7')
+
+        cy.get('#id_nota1_materia2').type('9.0')
+        cy.get('#id_nota2_materia2').type('9.1')
+        cy.get('#id_nota3_materia2').type('9.3')
+
+        cy.get('#id_nota1_materia3').type('10.0')
+        cy.get('#id_nota2_materia3').type('9.6')
+        cy.get('#id_nota3_materia3').type('9.8')
+
+        cy.get('#id_nota1_materia4').type('8.1')
+        cy.get('#id_nota2_materia4').type('8.8')
+        cy.get('#id_nota3_materia4').type('9.5')
+
+        cy.get('.default').click()
+
         cy.visit('/');
         cy.get('[href="/aluno/"] > .botao').click()
         cy.get('#username').type('59843216754')
@@ -22,6 +132,38 @@ describe('test visualizar Boletim', () => {
     })
 
     it('cenario3', () => { //André Sales está cadastrado no sistema da SmartSchool com os seguintes dados: matrícula - 20240506; CPF - 12528976549; data de nascimento - 18/01/2017; endereço - Rua da Hora, 45; e-mail - paulosales@gmail.com; nome do responsável - Paulo Henrique Sales; CPF do responsável - 02399752827; número de telefone - 81940148945. Ele está matriculado nas aulas do 2° ano A do Fundamental I, que inclui as matérias de História II, Geografia II, Matemática II, Português II e Ciências II. Ele tentará consultar suas notas antes que o professor as tenha cadastrado e aparecerá a página vazia.
+
+        cy.visit('/admin'); //pagina http://127.0.0.1:8000/
+        cy.get('#id_username').type('admin')
+        cy.get('#id_password').type('123456')
+        cy.get('.submit-row > input').click()
+        cy.get('.model-user > :nth-child(2) > .addlink').click()
+        cy.get('#id_username').type('12528976549')
+        cy.get('#id_password1').type('and20240506')
+        cy.get('#id_password2').type('and20240506')
+        cy.get('.default').click()
+
+        cy.get('.model-useraluno > td > .addlink')
+        cy.get('.model-useraluno > td > .addlink').click()
+        cy.get('#id_user').select('12528976549');
+        cy.get('#id_matricula').type('and20240506')
+        cy.get('#id_nome').type('André Sales')
+        cy.get('#id_cpf').type('12528976549')
+        cy.get('#id_data_de_nascimento').type('18/01/2017')
+        cy.get('#id_endereco').type(' Rua da Hora, 45')
+        cy.get('#id_colegio').type('Saber Viver')
+        cy.get('#id_serie').type('2')
+        cy.get('#id_turma').type('A')
+        cy.get('#id_email').type('paulosales@gmail.com')
+        cy.get('#id_nome_responsavel').type('Paulo Henrique Sales')
+        cy.get('#id_cpf_responsavel').type('02399752827')
+        cy.get('#id_telefone').type('81940148945')
+        cy.get('#id_email_responsavel').type('paulosales@gmail.com')
+        cy.get('.default').click()
+
+        cy.get('.model-notas > th').click()
+        cy.get('li > .addlink').click()
+
 
         cy.visit('/');
         cy.get('[href="/aluno/"] > .botao').click()
