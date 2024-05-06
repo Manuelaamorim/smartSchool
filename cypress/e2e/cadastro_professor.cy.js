e62b13d40c3de902cb11b964cd3519c3fecd7a3c
luzianes <luzianes@gmail.com>
2024-05-06 19:05:03 -0300

    Ajustes no teste de cadastro do professor - diferentes funcionários

--- cypress/e2e/cadastro_professor.cy.js
+++ cypress/e2e/cadastro_professor.cy.js
@@ -49,13 +49,40 @@ describe('test cadastrar professor', () => {
         
     })
 
-    it('cenario2', () => { //A funcionária Stella Natalia Bernardes, cadastrada no sistema, acessa a aplicação e informa login (34479066420) e senha (stella123) e acessa o menu para cadastrar um professor. Informa os dados do professor a ser cadastrado (nome - Maria Alice Gusmão, matrícula - malgu123, CPF - 84936329452, data de nascimento - 1997-11-07, telefone - 81961876432 e email - malicegusmao@outlook.com), clica no botão "Cadastrar Professor" e o cadastro é finalizado com sucesso. Em seguida, tenta cadastrar um novo professor, informa seus dados (nome - Manuel Fernandes da Silva, CPF - 84936329452, matrícula - mfds1234, data de nascimento - 1996-07-11, telefone 8196880909 e email - mfds@example.com) e o cadastro não é realizado, pois já há um professor cadastrado no sistema com o mesmo CPF.
+    it('cenario2', () => { //A funcionária Maria da Conceição Xavier, cadastrada no sistema, acessa a aplicação e informa login (34479066420) e senha (stella123) e acessa o menu para cadastrar um professor. Informa os dados do professor a ser cadastrado (nome - Maria Alice Gusmão, matrícula - malgu123, CPF - 84936329452, data de nascimento - 1997-11-07, telefone - 81961876432 e email - malicegusmao@outlook.com), clica no botão "Cadastrar Professor" e o cadastro é finalizado com sucesso. Em seguida, tenta cadastrar um novo professor, informa seus dados (nome - Manuel Fernandes da Silva, CPF - 84936329452, matrícula - mfds1234, data de nascimento - 1996-07-11, telefone 8196880909 e email - mfds@example.com) e o cadastro não é realizado, pois já há um professor cadastrado no sistema com o mesmo CPF.
 
-        //Acessa a página http://127.0.0.1:8000/, faz login como funcionário Stella e para cadastrar a professora Maria Alice Gusmão
+        //Acessa o admin com o superuser cadastrado e cadastra um funcionário
+
+        cy.visit('/admin'); // http://127.0.0.1:8000/
+        cy.get('#id_username').type('admin')
+        cy.get('#id_password').type('123456')
+        cy.get('.submit-row > input').click()
+        cy.get('.model-user > :nth-child(2) > .addlink').click()
+        cy.get('#id_username').type('84258399477')
+        cy.get('#id_password1').type('mariacx123')
+        cy.get('#id_password2').type('mariacx123')
+        cy.get('.default').click()
+        cy.wait(3000)
+        
+        cy.get('.model-userfuncionario > td > .addlink')
+        cy.get('.model-userfuncionario > td > .addlink').click()
+        cy.get('#id_user').select('84258399477');
+        cy.get('#id_nome').type('Maria da Conceição Xavier')
+        cy.get('#id_matricula').type('mariacx123')
+        cy.get('#id_cpf').type('84258399477')
+        cy.get('#id_data_de_nascimento').type('11/02/1984')
+        cy.get('#id_telefone').type('81982355958')
+        cy.get('#id_email').type('mariacx@gmail.com')
+        cy.get('.default').click()
+        cy.wait(3000)
+
+        cy.get('#logout-form > button').click()
+
+        //Acessa a página http://127.0.0.1:8000/, faz login como funcionário Maria da Conceição Xavier e para cadastrar a professora Maria Alice Gusmão
         cy.visit('/');
         cy.get('[href="/funcionario/"] > .botao').click()
-        cy.get('#username').type('34479066420')
-        cy.get('#password').type('stellanb123')
+        cy.get('#username').type('84258399477')
+        cy.get('#password').type('mariacx123')
         cy.get('.btn-submit').click()
         cy.wait(3000)
         cy.get(':nth-child(2) > .nav-menu-texto').click()
@@ -95,8 +122,34 @@ describe('test cadastrar professor', () => {
         
     })
 
-    it('cenario3', () => { //A funcionária Stella Natalia Bernardes, cadastrada no sistema, acessa a aplicação e informa login (34479066420) e senha (stella123) e acessa o menu para cadastrar um professor. Deixa um ou mais campos em branco e clica no botão "Cadastrar Professor", mas o formulário não é submetido, pois todos os campos devem ser preenchidos. Ao informar todos os campos do professor a ser cadastrado (nome - Maria das Dores Pascoal, matrícula - mdopa123, CPF - 98765432100, data de nascimento - 1975-02-03, telefone - 81984653875 e email - mdasdorespascoal@hotmail.com), clica novamente no botão "Cadastrar Professor" e o cadastro é finalizado com sucesso.
+    it('cenario3', () => { //A funcionária Clarice Maria da Silva, cadastrada no sistema, acessa a aplicação e informa login (34479066420) e senha (stella123) e acessa o menu para cadastrar um professor. Deixa um ou mais campos em branco e clica no botão "Cadastrar Professor", mas o formulário não é submetido, pois todos os campos devem ser preenchidos. Ao informar todos os campos do professor a ser cadastrado (nome - Maria das Dores Pascoal, matrícula - mdopa123, CPF - 98765432100, data de nascimento - 1975-02-03, telefone - 81984653875 e email - mdasdorespascoal@hotmail.com), clica novamente no botão "Cadastrar Professor" e o cadastro é finalizado com sucesso.
 
+        //Acessa o admin com o superuser cadastrado e cadastra um funcionário
+
+        cy.visit('/admin'); // http://127.0.0.1:8000/
+        cy.get('#id_username').type('admin')
+        cy.get('#id_password').type('123456')
+        cy.get('.submit-row > input').click()
+        cy.get('.model-user > :nth-child(2) > .addlink').click()
+        cy.get('#id_username').type('27307763460')
+        cy.get('#id_password1').type('clams123')
+        cy.get('#id_password2').type('clams123')
+        cy.get('.default').click()
+        cy.wait(3000)
+         
+        cy.get('.model-userfuncionario > td > .addlink')
+        cy.get('.model-userfuncionario > td > .addlink').click()
+        cy.get('#id_user').select('27307763460');
+        cy.get('#id_nome').type('Clarice Maria da Silva')
+        cy.get('#id_matricula').type('clams123')
+        cy.get('#id_cpf').type('27307763460')
+        cy.get('#id_data_de_nascimento').type('11/03/1984')
+        cy.get('#id_telefone').type('81982350000')
+        cy.get('#id_email').type('clams@gmail.com')
+        cy.get('.default').click()
+        cy.wait(3000)
+ 
+        cy.get('#logout-form > button').click()
 
        //Acessa a página http://127.0.0.1:8000/ e faz login como funcionário Stella Bernardes para cadastrar a professora Maria das Dores Pascoal
 
