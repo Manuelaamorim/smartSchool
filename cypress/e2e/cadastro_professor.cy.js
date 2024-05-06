6941df2c77fac2848e575da87130af16f93fb458
luzianes <luzianes@gmail.com>
2024-05-06 16:44:30 -0300

    Ajustes nos testes do cadastro de professor (cenários 2 e 3)

--- cypress/e2e/cadastro_professor.cy.js
+++ cypress/e2e/cadastro_professor.cy.js
@@ -28,7 +28,7 @@ describe('test cadastrar professor', () => {
 
         cy.get('#logout-form > button').click()
         
-        //Acessa a página http://127.0.0.1:8000/, faz login como funcionário Stella e cadastra o professor José Manuel Barros com sucesso
+        //Acessa a página http://127.0.0.1:8000/, faz login como funcionário Stella Bernardes e cadastra o professor José Manuel Barros com sucesso
 
         cy.visit('/');
         cy.get('[href="/funcionario/"] > .botao').click()
@@ -49,10 +49,9 @@ describe('test cadastrar professor', () => {
         
     })
 
-    it('cenario2', () => { //A funcionária Stella Natalia Bernardes, cadastrada no sistema, acessa a aplicação e informa login (34479066420) e senha (stella123) e acessa o menu para cadastrar um professor. Informa os dados do professor a ser cadastrado (nome - Maria Alice Gusmão, matrícula - malgu123, CPF - 94671686422, data de nascimento - 1997-11-07, telefone - 81961876432 e email - malicegusmao@outlook.com), clica no botão "Cadastrar Professor" e o cdastro não é realizado, pois já há um professor cadastrado com o mesmo CPF no sistema.
+    it('cenario2', () => { //A funcionária Stella Natalia Bernardes, cadastrada no sistema, acessa a aplicação e informa login (34479066420) e senha (stella123) e acessa o menu para cadastrar um professor. Informa os dados do professor a ser cadastrado (nome - Maria Alice Gusmão, matrícula - malgu123, CPF - 84936329452, data de nascimento - 1997-11-07, telefone - 81961876432 e email - malicegusmao@outlook.com), clica no botão "Cadastrar Professor" e o cadastro é finalizado com sucesso. Em seguida, tenta cadastrar um novo professor, informa seus dados (nome - Manuel Fernandes da Silva, CPF - 84936329452, matrícula - mfds1234, data de nascimento - 1996-07-11, telefone 8196880909 e email - mfds@example.com) e o cadastro não é realizado, pois já há um professor cadastrado no sistema com o mesmo CPF.
 
-        //Acessa a página http://127.0.0.1:8000/, faz login como funcionário Stella e tenta cadastrar novamente o professor José Manuel Barros
-
+        //Acessa a página http://127.0.0.1:8000/, faz login como funcionário Stella e para cadastrar a professora Maria Alice Gusmão
         cy.visit('/');
         cy.get('[href="/funcionario/"] > .botao').click()
         cy.get('#username').type('34479066420')
@@ -62,20 +61,44 @@ describe('test cadastrar professor', () => {
         cy.get(':nth-child(2) > .nav-menu-texto').click()
         cy.get('#nome').type('Maria Alice Gusmão')
         cy.get('#matricula').type('malgu123')
-        cy.get('#cpf').type('94671686422')
+        cy.get('#cpf').type('84936329452')
         cy.get('#data_de_nascimento').type('1997-11-07')
         cy.get('#telefone').type('81961876432')
         cy.get('#email').type('malicegusmao@outlook.com')
         cy.get('.btn-submit').click()
+        cy.get('p').invoke('text').should('have.string', "Cadastro realizado com sucesso.")
+        
+        cy.wait(3000)
+
+        cy.get(':nth-child(2) > .nav-menu-texto').click()
+        cy.get('#nome').type('Manuel Fernandes da Silva')
+        cy.get('#matricula').type('mfds1234')
+        cy.get('#cpf').type('84936329452')
+        cy.get('#data_de_nascimento').type('1996-07-11')
+        cy.get('#telefone').type('8196880909')
+        cy.get('#email').type('mfds@example.com')
+        cy.get('.btn-submit').click()
         cy.get('.error').invoke('text').should('have.string', "Um usuário com este CPF já está cadastrado.")
+        
+        cy.wait(3000)
+
+        cy.get('#nome').type('Manuel Fernandes da Silva')
+        cy.get('#matricula').type('mfds1234')
+        cy.get('#cpf').type('90990949494')
+        cy.get('#data_de_nascimento').type('1996-07-11')
+        cy.get('#telefone').type('8196880909')
+        cy.get('#email').type('mfds@example.com')
+        cy.get('.btn-submit').click()
+        cy.get('p').invoke('text').should('have.string', "Cadastro realizado com sucesso.")
+        
         cy.wait(3000)
         
     })
 
-    it('cenario3', () => { //A funcionária Stella Natalia Bernardes, cadastrada no sistema, acessa a aplicação e informa login (34479066420) e senha (stella123) e acessa o menu para cadastrar um professor. Informa apenas o nome do professor e clica no botão "Cadastrar Professor", mas o formulário não é submetido, pois todos os campos devem ser preenchidos. Ao informar todos os campos do professor a ser cadastrado (nome - Maria das Dores Pascoal, matrícula - mdopa123, CPF - 98765432100, data de nascimento - 1975-02-03, telefone - 81984653875 e email - mdasdorespascoal@hotmail.com), clica novamente no botão "Cadastrar Professor" e o cadastro é finalizado com sucesso.
+    it('cenario3', () => { //A funcionária Stella Natalia Bernardes, cadastrada no sistema, acessa a aplicação e informa login (34479066420) e senha (stella123) e acessa o menu para cadastrar um professor. Deixa um ou mais campos em branco e clica no botão "Cadastrar Professor", mas o formulário não é submetido, pois todos os campos devem ser preenchidos. Ao informar todos os campos do professor a ser cadastrado (nome - Maria das Dores Pascoal, matrícula - mdopa123, CPF - 98765432100, data de nascimento - 1975-02-03, telefone - 81984653875 e email - mdasdorespascoal@hotmail.com), clica novamente no botão "Cadastrar Professor" e o cadastro é finalizado com sucesso.
 
 
-       //Acessa a página http://127.0.0.1:8000/ e faz login como funcionário Stella
+       //Acessa a página http://127.0.0.1:8000/ e faz login como funcionário Stella Bernardes para cadastrar a professora Maria das Dores Pascoal
 
         cy.visit('/')
         cy.get('[href="/funcionario/"] > .botao').click()
@@ -99,9 +122,31 @@ describe('test cadastrar professor', () => {
 
         cy.wait(5000)
 
-        // Preenche os demais campos requeridos
+        // Preenche o campo matrícula
         cy.get('#matricula').type('mdopa123');
+
+        // Tenta submeter o formulário
+        cy.get('.btn-submit').click();
+
+        // Verifica se a URL mudou para a página de sucesso, o que indica que o formulário foi submetido
+        // Não houve mudança, pois os outros campos requeridos não foram preenchidos
+        cy.url().should('not.include', '/cadastro_sucesso');
+
+        cy.wait(5000)
+
+        // Preenche o campo CPF
         cy.get('#cpf').type('98765432100');
+
+        // Tenta submeter o formulário
+        cy.get('.btn-submit').click();
+
+        // Verifica se a URL mudou para a página de sucesso, o que indica que o formulário foi submetido
+        // Não houve mudança, pois os outros campos requeridos não foram preenchidos
+        cy.url().should('not.include', '/cadastro_sucesso');
+
+        cy.wait(5000)
+
+        //Preenche os demais campos
         cy.get('#data_de_nascimento').type('1975-02-03');
         cy.get('#telefone').type('81984653875');
         cy.get('#email').type('mdasdorespascoal@hotmail.com');
