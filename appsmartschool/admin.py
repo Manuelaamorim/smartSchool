4691bf484da585bd9e8c2217100e0e455bce758e
MatheusVelame <mvp2@cesar.school>
2024-05-12 23:32:46 -0300

    ajustando o admin

--- appsmartschool/admin.py
+++ appsmartschool/admin.py
@@ -1,14 +1,7 @@
 from django.contrib import admin
 
-from .models import UserAluno
-from .models import UserProfessor
-from .models import UserFuncionario
-from .models import Dados_saude
-from .models import MensagemContato
-from .models import Frequencia_Aluno
-from .models import HorarioAula
-from .models import Notas
-from .models import Disciplina
+from .models import *
+
 
 admin.site.register(UserAluno)
 admin.site.register(UserProfessor)
