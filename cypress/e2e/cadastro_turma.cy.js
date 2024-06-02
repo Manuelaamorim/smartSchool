a8a5fb2f8d48745d449f55e42d0b84803a018a62
MatheusVelame <mvp2@cesar.school>
2024-06-01 23:54:45 -0300

    ajustes turma

--- cypress/e2e/cadastro_turma.cy.js
+++ cypress/e2e/cadastro_turma.cy.js
@@ -69,7 +69,7 @@ describe('test cadastrar Turma', () => {
         cy.get('#codigo_materia_4').type('2024ART4');
         cy.get('.btn-submit').click();
 
-        cy.get('p').invoke('text').should('have.string', "Cadastro realizado com sucesso.")
+        cy.contains('p', 'Cadastro realizado com sucesso.', { timeout: 10000 }).should('be.visible');
     });
 
     it('cenario2', () => {
@@ -179,7 +179,9 @@ describe('test cadastrar Turma', () => {
         cy.get('#codigo_materia_4').type('2025ART4');
         cy.get('.btn-submit').click();
 
-        cy.get('.error').invoke('text').should('have.string', "A turma já existe.")
+        cy.get('.messages', { timeout: 10000 }).within(() => {
+            cy.contains('div', 'A turma já existe.').should('be.visible');
+        });
     });
 
     it('cenario3', () => {
@@ -252,7 +254,9 @@ describe('test cadastrar Turma', () => {
         cy.get('#codigo_materia_4').type('2024ART4');
         cy.get('.btn-submit').click();
 
-        cy.get('.error').invoke('text').should('have.string', "Matéria 1 não cadastrada.")
+        cy.get('.messages', { timeout: 10000 }).within(() => {
+            cy.contains('div', 'Matéria 1 não cadastrada.').should('be.visible');
+        });
     });
 
     it('cenario4', () => {
@@ -355,6 +359,8 @@ describe('test cadastrar Turma', () => {
         cy.get('#codigo_materia_4').type('2025ART4');
         cy.get('.btn-submit').click();
 
-        cy.get('.error').invoke('text').should('have.string', "Código da materia 1 já existe.")
+        cy.get('.messages', { timeout: 10000 }).within(() => {
+            cy.contains('div', 'Código da materia 1 já existe.').should('be.visible');
+        });
     });
 });
